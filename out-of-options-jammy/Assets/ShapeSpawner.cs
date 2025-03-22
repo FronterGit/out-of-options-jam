3cedc9e4ce92ef3dc49b894d43915208b4456b99
Fronter <mathijs.klingeman@gmail.com>
2025-03-22 18:50:31 +0100

    shape spawner

---
+++ out-of-options-jammy/Assets/ShapeSpawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeSpawner : MonoBehaviour
+{
+    public List<Shape> shapes;
+    public int spawnTime = 2;
+    public Transform destination;
+    
+    private void Start()
+    {
+        StartCoroutine(SpawnShapes());
+    }
+    
+    private IEnumerator SpawnShapes()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnTime);
+            int randomShapeIndex = Random.Range(0, shapes.Count);
+            Shape shape = shapes[randomShapeIndex];
+            GameObject newShape = Instantiate(shape.gameObject, transform.position, Quaternion.identity);
+            var shapeComponent = newShape.GetComponent<Shape>();
+            
+            //give the shape a destination
+            shapeComponent.destination = destination;
+            shapeComponent.travelling = true;
+        }
+    }
+}
